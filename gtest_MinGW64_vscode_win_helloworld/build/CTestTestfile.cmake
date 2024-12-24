545d7c97372957224802d5ede097728abc495016
Gordon Y <gordon.yeung.toa@gmail.com>
2024-12-23 19:03:37 -0800

    chore: Remove build folder from version control

--- gtest_MinGW64_vscode_win_helloworld/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Gordon/Documents/Developer/2024_embedded_c++/gtest_MinGW64_vscode_win_helloworld
-# Build directory: C:/Users/Gordon/Documents/Developer/2024_embedded_c++/gtest_MinGW64_vscode_win_helloworld/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(MyHelloWorldTest "C:/Users/Gordon/Documents/Developer/2024_embedded_c++/gtest_MinGW64_vscode_win_helloworld/build/tests.exe")
-set_tests_properties(MyHelloWorldTest PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/Gordon/Documents/Developer/2024_embedded_c++/gtest_MinGW64_vscode_win_helloworld/CMakeLists.txt;27;add_test;C:/Users/Gordon/Documents/Developer/2024_embedded_c++/gtest_MinGW64_vscode_win_helloworld/CMakeLists.txt;0;")
