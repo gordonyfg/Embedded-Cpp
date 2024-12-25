4a4db9c88aee01b60dd00dc76705889683564d0c
Gordon Y <gordon.yeung.toa@gmail.com>
2024-12-24 22:56:45 -0800

    fix test

--- tests/test_LC167.cpp
+++ tests/test_LC167.cpp
@@ -2,15 +2,17 @@
 #include "LC167.h"
 
 TEST(LC167, ExampleTest) {
+    Solution solution;
     std::vector<int> input = {2, 7, 11, 15};
     int target = 9;
     std::vector<int> expected = {1, 2};
-    EXPECT_EQ(twoSum(input, target), expected);
+    EXPECT_EQ(solution.twoSum(input, target), expected);
 }
 
 TEST(LC167, EdgeCaseTest) {
+    Solution solution;
     std::vector<int> input = {1, 2};
     int target = 3;
     std::vector<int> expected = {1, 2};
-    EXPECT_EQ(twoSum(input, target), expected);
+    EXPECT_EQ(solution.twoSum(input, target), expected);
 }
